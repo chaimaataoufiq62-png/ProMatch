7e5f1a1d841e17d4d212f86095f0f5e07ccec000
Chaimaa Taoufiq <chaimaataoufiq62@gmail.com>
2026-03-07 04:14:04 +0100

    Updates

--- backend/controllers/candidateController.js
+++ backend/controllers/candidateController.js
@@ -5,7 +5,7 @@ exports.getProfile = async (req,res)=>{
  const userId = req.user.id;
 
  const [user] = await db.execute(
-  "SELECT id,name,email FROM users WHERE id=?",
+  "SELECT id,name,email FROM utilisateur WHERE id=?",
   [userId]
  );
 
