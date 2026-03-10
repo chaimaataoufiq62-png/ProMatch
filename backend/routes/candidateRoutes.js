8b13fd1314092086c10b1e4d8687d3ce1e65c694
Chaimaa Taoufiq <chaimaataoufiq62@gmail.com>
2026-03-10 15:30:51 +0100

    Updates

--- backend/routes/candidateRoutes.js
+++ backend/routes/candidateRoutes.js
@@ -5,5 +5,8 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const candidateController = require("../controllers/candidateController");
 
 router.get("/profile", authMiddleware, candidateController.getProfile);
+router.post("/skills", authMiddleware, candidateController.addSkill);
+router.get("/skills", authMiddleware, candidateController.getSkills);
+router.delete("/skills/:id", authMiddleware, candidateController.deleteSkill);
 
 module.exports = router;
